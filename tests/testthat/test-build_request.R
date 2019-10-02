245e40d04cf9ba1daad9ae142c3d7de6510cee7e
Charlotte Wickham <cwickham@gmail.com>
2019-10-02 08:28:01 -0700

    add explicit domain to tests too

--- tests/testthat/test-build_request.R
+++ tests/testthat/test-build_request.R
@@ -12,17 +12,22 @@ test_that("all forms of specifying endpoint are equivalent", {
 })
 
 test_that("method arg sets default method", {
-  r <- gh_build_request("/courses", method = "POST", token = "")
+  r <- gh_build_request("/courses", method = "POST", token = "",
+    api_url = "https://canvas.instructure.com/api/v1")
   expect_equal(r$method, "POST")
 })
 
 test_that("parameter substitution is equivalent to direct specification", {
   subst <-
     gh_build_request("POST /repos/:org/:repo/issues/:number/labels",
-                     params = list(org = "ORG", repo = "REPO", number = "1",
-                                   "body"), token = "")
+      params = list(org = "ORG", repo = "REPO", number = "1",
+                    "body"),
+      token = "",
+      api_url = "https://canvas.instructure.com/api/v1")
   spec <-
     gh_build_request("POST /repos/ORG/REPO/issues/1/labels",
-                     params = list("body"), token = "")
+      params = list("body"),
+      token = "",
+      api_url = "https://canvas.instructure.com/api/v1")
   expect_identical(subst, spec)
 })
